2e19deb3bff80aa12e80c920e6864f57c890dcc5
sho171 <sh.zxcv00@gmail.com>
2020-12-23 15:05:01 +0900

    Add: 静的ページ追加（仮）

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,13 @@
+class StaticPagesController < ApplicationController
+  def top
+  end
+
+  def terms
+  end
+
+  def privacy
+  end
+
+  def help
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'static_pages#top'
+  get '/terms', to: 'static_pages#terms'
+  get '/privacy', to: 'static_pages#privacy'
+  get '/help', to: 'static_pages#help'
 end
