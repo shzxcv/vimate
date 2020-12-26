27edede9b5cc18f1ebc359d368653690094decb2
sho171 <sh.zxcv00@gmail.com>
2020-12-27 02:27:56 +0900

    Modify: セッション時間延期

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,1 @@
-Rails.application.config.session_store :cookie_store, key: '_vimate_session', expire_after: 1.day
+Rails.application.config.session_store :cookie_store, key: '_vimate_session', expire_after: 7.day
