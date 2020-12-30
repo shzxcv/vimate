8c3af16e21e87da4dd7607194d757024487f9aff
sho171 <sh.zxcv00@gmail.com>
2020-12-30 19:48:53 +0900

    Add: UserLessonテーブル追加

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,5 +1,7 @@
 class Lesson < ApplicationRecord
   has_many :commands, dependent: :destroy
+  has_many :user_lessons, dependent: :destroy
+  has_many :users, through: :user_lessons
 
   validates :name, presence: true, uniqueness: { scope: :category }
   validates :category, presence: true
