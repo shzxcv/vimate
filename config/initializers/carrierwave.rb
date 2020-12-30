1c11fba1592f18f3cf34942ddb39dc0ae29f7f63
sho171 <sh.zxcv00@gmail.com>
2020-12-30 17:42:49 +0900

    Modify: s3 iam settings

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,9 +2,9 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: Rails.application.credentials.aws[:s3_key],
-      aws_secret_access_key: Rails.application.credentials.aws[:s3_secret],
-      region: Rails.application.credentials.aws[:s3_region]
+      aws_access_key_id: "#{Rails.application.credentials.aws[:s3_key]}",
+      aws_secret_access_key: "#{Rails.application.credentials.aws[:s3_secret]}",
+      region: "#{Rails.application.credentials.aws[:s3_region]}"
     }
 
     config.fog_directory  = 'rails_user_avatar'
