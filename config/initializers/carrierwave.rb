36452e05f55302c65681224862564a85f33fb791
EC2 Default User <ec2-user@ip-10-0-1-36.ap-northeast-1.compute.internal>
2020-12-30 19:00:50 +0900

    Modify: S3 production

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,15 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: "#{Rails.application.credentials.aws[:s3_key]}",
-      aws_secret_access_key: "#{Rails.application.credentials.aws[:s3_secret]}",
-      region: "#{Rails.application.credentials.aws[:s3_region]}"
-    }
-
-    config.fog_directory  = 'rails_user_avatar'
+    config.fog_directory  = Rails.application.credentials.aws[:s3_directory]
     config.cache_storage = :fog
     config.fog_public = false
     config.fog_authenticated_url_expiration = 60
+
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: Rails.application.credentials.aws[:s3_key],
+      aws_secret_access_key: Rails.application.credentials.aws[:s3_secret],
+      region: Rails.application.credentials.aws[:s3_region]
+    }
   end
 end
--- config/puma/production.rb
+++ config/puma/production.rb
@@ -13,7 +13,7 @@ preload_app!
 # デーモン化の設定
 # daemonize
 pidfile "#{tmp_path}/pids/puma.pid"
-# stdout_redirect "#{tmp_path}/logs/puma.stdout.log", "#{tmp_path}/logs/puma.stderr.log", true
+stdout_redirect "#{tmp_path}/logs/puma.stdout.log", "#{tmp_path}/logs/puma.stderr.log", true
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
