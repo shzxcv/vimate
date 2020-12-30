f548791ab438c8ce1a185ee5a23eb192067bb5b3
EC2 Default User <ec2-user@ip-10-0-1-36.ap-northeast-1.compute.internal>
2020-12-30 20:07:00 +0900

    Add: Puma Log

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -3,6 +3,7 @@ environment "production"
 
 # UNIX Socketへのバインド
 tmp_path = "#{File.expand_path("../../..", __FILE__)}/tmp"
+log_path = "#{File.expand_path("../../..", __FILE__)}/log"
 bind "unix://#{tmp_path}/sockets/puma.sock"
 
 # スレッド数とWorker数の指定
@@ -13,7 +14,7 @@ preload_app!
 # デーモン化の設定
 # daemonize
 pidfile "#{tmp_path}/pids/puma.pid"
-stdout_redirect "#{tmp_path}/logs/puma.stdout.log", "#{tmp_path}/logs/puma.stderr.log", true
+stdout_redirect "#{log_path}/puma.stdout.log", "#{log_path}/puma.stderr.log", true
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
