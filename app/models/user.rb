f20b4b1f2d0aa559146fc764f0363b19be5f91d3
sho171 <sh.zxcv00@gmail.com>
2021-01-05 18:51:10 +0900

    Add: user role

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,8 @@ class User < ApplicationRecord
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
 
+  enum role: { general: 0, admin: 1}
+
   def to_param
     name
   end
