9c4f66cd6ea1f86d4045c84060c3df518e843eec
sho171 <sh.zxcv00@gmail.com>
2021-01-08 22:32:48 +0900

    Modify: vimmer 画像

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -7,15 +7,15 @@ class LessonsController < ApplicationController
 
   def show
     @lesson = Lesson.find_by!(url: params[:url])
-    if @lesson.url.match?("vimmer")
+    if @lesson.url.match?("vimmer-01")
+      gon.vimuta_src_url = helpers.asset_path 'vimmer_01_vimuta.png'
+      gon.block_src_url = helpers.asset_path 'vimmer_01_block_emacs.png'
+      gon.goal_src_url = helpers.asset_path 'vimmer_01_goal.png'
       render @lesson.url.gsub('-', '_')
       return
     end
     @commands = @lesson.commands.sample(3)
     gon.commands = @commands
     logged_in? ? gon.user_login_flag = true : gon.user_login_flag = false
-  end
-
-  def vimmer_01
   end
 end
