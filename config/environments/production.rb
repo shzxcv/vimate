a62112fe62f31db0e4fbd9a0ed6c6f047f6b6e3c
sho171 <sh.zxcv00@gmail.com>
2021-01-10 13:24:52 +0900

    Add: production mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -112,6 +112,7 @@ Rails.application.configure do
 
   #Gmail設定
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'vimate.jp' }
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: '587',
