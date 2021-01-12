c11724048f90f2d70f5b96af553d79e79bfae209
sho171 <sh.zxcv00@gmail.com>
2021-01-13 02:16:56 +0900

    Add: static page design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   def default_meta_tags
     {
       site: 'Vimate',
-      title: '',
+      title: 'Vim特化学習サイト',
       reverse: false,
       charset: 'utf-8',
       description: 'VimateはVimに特化した学習サイトです。ゲーム形式でVimを学び、Vimmerへの道を歩みましょう。',
