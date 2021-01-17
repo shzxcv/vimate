00c2a434d334622dbe529741008c62e93f92ab98
sho171 <sh.zxcv00@gmail.com>
2021-01-17 15:05:33 +0900

    Add: マイページVimmer階級制度導入

--- db/fixtures/01_lessons.rb
+++ db/fixtures/01_lessons.rb
@@ -1,11 +1,8 @@
 Lesson.seed(:id,
-  {id: 1, name: '初級編', category: 'Vim', description: 'Vim初級編は基礎的なコマンドを行います。', url: 'vim-01'},
-  {id: 2, name: '中級編', category: 'Vim', description: 'Vim中級編は実用的なコマンドを行います。', url: 'vim-02'},
-  {id: 3, name: '上級編', category: 'Vim', description: 'Vim上級編を知っていたらVimマスター間違い無し！', url: 'vim-03'},
-  {id: 4, name: '初級編', category: 'Vimrc', description: 'Vimrc初級編は基礎的なコマンドを行います。', url: 'vimrc-01'},
-  {id: 5, name: '中級編', category: 'Vimrc', description: 'Vimrc中級編は実用的なコマンドを行います。', url: 'vimrc-02'},
-  {id: 6, name: '上級編', category: 'Vimrc', description: 'Vimrc上級編を知っていたらVimマスター間違い無し！', url: 'vimrc-03'},
-  {id: 7, name: '初級編', category: 'Option', description: 'Vimmer診断初級編でVimmer力を測ろう！', url: 'vimmer-01'},
-  {id: 8, name: '中級編', category: 'Option', description: '近日リリース予定!!', url: 'vimmer-02'},
-  {id: 9, name: '上級編', category: 'Option', description: '近日リリース予定!!', url: 'vimmer-03'}
+  {id: 1, name: '初級編', category: 'Vim', description: 'Vim初級編は移動やインサートなど基本的なコマンドを行います。解答に自信が無い方は、先にコマンド一覧を参照してからレッスンを行ってみてください。びむ太と穏やかにレッスンを行いましょう。', url: 'vim-01'},
+  {id: 2, name: '中級編', category: 'Vim', description: 'Vim中級編はテキストオブジェクトや検索など知っておくべきなコマンドを行います。解答に自信が無い方は、先にコマンド一覧を参照してからレッスンを行ってみてください。びむ太を火山から救い出してください。', url: 'vim-02'},
+  {id: 3, name: '上級編', category: 'Vim', description: 'Vim上級編を知っていたらVimマスター間違い無し！解答に自信が無い方は、先にコマンド一覧を参照してからレッスンを行ってみてください。宇宙の無酸素でびむ太は倒れかけています。一刻も早く解答してびむ太を救い出しましょう。', url: 'vim-03'},
+  {id: 4, name: '初級編', category: 'Option', description: 'Vimmer診断初級編でVimmer力を測ろう！', url: 'vimmer-01'},
+  {id: 5, name: '中級編', category: 'Option', description: '近日リリース予定!!', url: 'vimmer-02'},
+  {id: 6, name: '上級編', category: 'Option', description: '近日リリース予定!!', url: 'vimmer-03'}
 )
