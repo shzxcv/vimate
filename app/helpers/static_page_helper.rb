96fcf78461753a9439fae4f37a21290df1d5e09a
sho171 <sh.zxcv00@gmail.com>
2021-01-19 19:36:51 +0900

    Modify: design

---
+++ app/helpers/static_page_helper.rb
@@ -0,0 +1,17 @@
+module StaticPageHelper
+  def title_size
+    request.from_pc? ? 'is-size-2' : 'is-size-4'
+  end
+
+  def sub_title_size
+    'is-size-4' if !request.from_pc?
+  end
+
+  def lesson_button
+    request.from_pc? ? 'mt-4' : 'is-small mt-3'
+  end
+
+  def submit_button
+    request.from_pc? ? 'w-50' : 'w-75'
+  end
+end
