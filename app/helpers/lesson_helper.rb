0c48bc220de25220e78b49b312f2227b96a1f615
sho171 <sh.zxcv00@gmail.com>
2021-01-20 00:25:04 +0900

    Add: モバイル デスクトップ表示対策

--- app/helpers/lesson_helper.rb
+++ app/helpers/lesson_helper.rb
@@ -18,4 +18,12 @@ module LessonHelper
       "background-color: #141651"
     end
   end
+
+  def only_pc?
+    if request.from_iphone? || request.from_ipad? || request.from_ipod? || request.from_android? || request.from_android_tablet? || request.from_windows_phone? || request.from_ios? || request.from_android_os?
+      return false
+    else
+      return true
+    end
+  end
 end
