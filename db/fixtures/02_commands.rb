5e510f7eb8e81f5374e2cf24f63cdc218a55c182
sho171 <sh.zxcv00@gmail.com>
2021-01-20 15:03:51 +0900

    Add: lesson question

--- db/fixtures/02_commands.rb
+++ db/fixtures/02_commands.rb
@@ -1,12 +1,14 @@
 require 'csv'
 
-commands = CSV.read('db/fixtures/vim01_commands.csv')
+for x in 1..3 do
+  commands = CSV.read("db/fixtures/vim0#{x}_commands.csv")
 
-commands.each do |command|
-  Command.seed do |s|
-    s.id = command[0]
-    s.lesson_id = command[1]
-    s.question = command[2]
-    s.answer_key = command[3]
+  commands.each do |command|
+    Command.seed do |s|
+      s.id = command[0]
+      s.lesson_id = command[1]
+      s.question = command[2]
+      s.answer_key = command[3]
+    end
   end
 end
