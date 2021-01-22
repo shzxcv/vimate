3a983882f923698fd1a0033055b89710ee4457d6
sho171 <sh.zxcv00@gmail.com>
2021-01-22 15:19:51 +0900

    Modify: 細かい文言の調整

--- db/fixtures/01_lessons.rb
+++ db/fixtures/01_lessons.rb
@@ -1,8 +1,8 @@
 Lesson.seed(:id,
-  {id: 1, name: '初級編', category: 'Vim', description: 'Vim初級編は移動やインサートなど基本的なコマンドを行います。解答に自信が無い方は、先にコマンド一覧を参照してからレッスンを行ってみてください。びむ太と穏やかにレッスンを行いましょう。', url: 'vim-01'},
-  {id: 2, name: '中級編', category: 'Vim', description: 'Vim中級編はテキストオブジェクトや検索など知っておくべきなコマンドを行います。解答に自信が無い方は、先にコマンド一覧を参照してからレッスンを行ってみてください。びむ太を火山から救い出してください。', url: 'vim-02'},
-  {id: 3, name: '上級編', category: 'Vim', description: 'Vim上級編を知っていたらVimマスター間違い無し！解答に自信が無い方は、先にコマンド一覧を参照してからレッスンを行ってみてください。宇宙の無酸素でびむ太は倒れかけています。一刻も早く解答してびむ太を救い出しましょう。', url: 'vim-03'},
-  {id: 4, name: '初級編', category: 'Option', description: 'Vimmer診断初級編でVimmer力を測ろう！', url: 'vimmer-01'},
-  {id: 5, name: '中級編', category: 'Option', description: '近日リリース予定!!', url: 'vimmer-02'},
-  {id: 6, name: '上級編', category: 'Option', description: '近日リリース予定!!', url: 'vimmer-03'}
+  {id: 1, name: '初級編', category: 'Vim', description: 'Vim初級編は移動やインサートなどVimの基本的なコマンドが出題されます。コマンド一覧やコマンド表示モードを使用して、まずはVimに慣れていきましょう！初級編をマスターすれば、Vimmerへの大きな一歩です！', url: 'vim-01'},
+  {id: 2, name: '中級編', category: 'Vim', description: 'Vim中級編はテキストオブジェクトや検索などのコマンドが出題されます。初級編と同様、コマンド一覧やコマンド表示モードが使用可能です。中級編までマスターすれば、Vimでの操作が楽しくなってくるはずです！', url: 'vim-02'},
+  {id: 3, name: '上級編', category: 'Vim', description: 'Vim上級編を知っていたらVimマスター間違い無し！これまでの問題よりも大幅に難易度が上がります。上級編もコマンド一覧やコマンド表示モードを使用することができます。上級編までマスターすれば、立派なVimmerの仲間入りです！', url: 'vim-03'},
+  {id: 4, name: '初級編', category: 'Option', description: 'Vimmer診断初級編は、上下左右の基本的なコマンドを使用して、迷路を突破します。Vim初心者の方からVimmerまで幅広く遊べる内容です。Vimmer診断で、自分のVimmer力を測ってみましょう！', url: 'vimmer-01'},
+  {id: 5, name: '中級編', category: 'Option', description: '近日リリース予定！', url: 'vimmer-02'},
+  {id: 6, name: '上級編', category: 'Option', description: '近日リリース予定！', url: 'vimmer-03'}
 )
