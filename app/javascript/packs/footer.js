c01c35313b95b518a783b2169af86658c0722a60
sho171 <sh.zxcv00@gmail.com>
2021-01-25 14:27:58 +0900

    Add: footer最下部固定

---
+++ app/javascript/packs/footer.js
@@ -0,0 +1,6 @@
+document.addEventListener("turbolinks:load", function(){
+  var $ftr = $('#footer');
+  if( window.innerHeight > $ftr.offset().top + $ftr.outerHeight() ){
+    $ftr.attr({'style': 'position:fixed; top:' + (window.innerHeight - $ftr.outerHeight()) +'px;' });
+  }
+})
