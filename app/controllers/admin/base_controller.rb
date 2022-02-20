c60b3907ef6832426ff5279f31d0975e7ec0569a
sho171 <sh.zxcv00@gmail.com>
2022-02-20 18:27:32 +0900

    add admin basic auth

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,8 +1,14 @@
 class Admin::BaseController < ApplicationController
-  before_action :check_admin
+  before_action :basic_auth, :check_admin
   layout 'admin/layouts/application'
 
   private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
+    end
+  end
 
   def check_admin
     redirect_to root_path unless current_user.admin?
